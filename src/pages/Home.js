46cde78e11b0dd2b8e7df114741981cfff41521c
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-04 14:35:03 -0400

    Implemented home component with chakra ui components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
-import Reac, { useEffect, useContext } from "react";
-
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
 import { ShopContext } from "../context/shopContext";
+import {
+    Box,
+    Grid,
+    Text,
+    Image
+} from "@chakra-ui/react"
 
 const Home = () => {
 
@@ -17,15 +23,21 @@ const Home = () => {
     if (!products) return <div>loading...</div>
 
     return (
-        <div>
-            {
-                products.map(product => (
-                    <a key={ product.title }>
-                        { product.title }
-                    </a>
-                ))
-            }
-        </div>
+        <Box>
+            <Grid templateColumns={["repeat(1fr)", "repeat(3, 1fr)"]}>
+                {products.map(product => (
+                <Link to={`/products/${product.handle}`} key={product.id} >
+                    <Box _hover={{ opacity: "80%" }} textAlign="center" position="relative">
+                    <Image
+                        src={product.images[0].src}
+                    />
+                    <Text fontWeight="bold" position="absolute" bottom="15%" w="100%">{product.title}</Text>
+                    <Text color="gray.500" position="absolute" bottom="5%" w="100%">${product.variants[0].price}</Text>
+                    </Box>
+                </Link>
+                ))}
+            </Grid>
+        </Box>
     )
 }
 
